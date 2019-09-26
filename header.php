e44843cbade98ed9c6d025f048982276125b4b94
Maxine Harris <discovermaxine@gmail.com>
2019-09-26 12:39:34 +0200

    Added styles, scripts, fonts and images
    Added "assets" folder from B2W static site
    
    Added updated Bootstrap files:
    1. Copied version 3.3.1 "bootstrap.min.css" and "bootstrap-theme.min.css" files over to the 3.3.0 (old) folder
    2. Replaced glyphicon font files with the files from version 3.3.1
    3. Copied version 3.3.1 "bootstrap.js" and "bootstrap.min.js" files over to the 3.3.0 (old) folder
    
    Added Boostrap, Font Awesome and Google Fonts references from B2W static site
    
    Placed theme specific styles in WordPress' "style.css" and removed "custom.css"
    
    Added HTML5 shiv code from B2W static site

--- header.php
+++ header.php
@@ -17,7 +17,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Bootstrap core CSS - NB: using file from completed project since it does not display correctly with the newest version of Bootstrap -->
+	<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/old/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Font Awesome Icons - NB: downloaded files did not work, using CDN instead -->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,600&display=swap" rel="stylesheet">
+
 	<?php wp_head(); ?>
+
+	<!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
