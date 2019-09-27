83a4ed4a38d3a3a8cc23a84ef38be864566fab12
Maxine Harris <discovermaxine@gmail.com>
2019-09-27 07:59:14 +0200

    Created a Home page template
    Added new file: page-home.php
    Used some code from page.php for the above file
    Created new page in the dashboard, setting its "Template" page attribute to "Home Page"
    Added code (from Hero to Testimonials) from static home page to page-home.php
    Added code from static footer to footer.php
    Made a correction to the footer (added ; for bloginfo php function)
    Altered the image paths (added <?php bloginfo('stylesheet_directory'); ?>/) on the home page and in the footer
    Edited "Permalink" settings in the dashboard (to display a more SEO friendly URL)
    Edited "Reading" settings in the dashboard (to display the Home page as the homepage - also prevented search engine visibility, which is important while still developing the site)
    Added more top-padding on #hero

--- footer.php
+++ footer.php
@@ -15,13 +15,85 @@
 
 <?php wp_footer(); ?>
 
+<!-- SIGN UP SECTION
+================================================== -->
+<section id="signup" data-type="background" data-speed="4">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Are you ready to take your coding skills to the <strong>next level</strong>?</h2>
+				<p><a href="" class="btn btn-lg btn-block btn-success">Yes, sign me up!</a></p>
+			</div><!-- end col -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- signup -->
+
+<!-- FOOTER
+================================================== -->
+<footer>
+	<div class="container">
+		<div class="col-sm-3">
+			<p><a href="/"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Bootstrap to WordPress"></a></p>
+		</div><!-- end col -->
+		<div class="col-sm-6">
+			<nav>
+				<ul class="list-unstyled list-inline">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="resources.html">Resources</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li class="signup-link"><a href="">Sign up now</a></li>
+				</ul>
+			</nav>
+		</div><!-- end col -->
+		<div class="col-sm-3">
+			<p class="pull-right">&copy; 2014 Brad Hussey</p>
+		</div><!-- end col -->
+	</div><!-- container -->
+</footer>
+
+<!-- MODAL
+================================================== -->
+<div class="modal fade" id="myModal">
+	<div class="modal-dialog">
+		<div class="modal-content">
+		
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i> Subscribe to our Mailing List</h4><!-- ERROR: Not displaying -->
+			</div><!-- modal-header -->
+			
+			<div class="modal-body">
+				<p>Simply enter your name and email! As a thank you for joining us, we're going to give you one of our best-selling courses, <em>for free!</em></p>
+				
+				<form class="form-inline" role="form">
+					<div class="form-group">
+						<label class="sr-only" for="subscribe-name">Your first name</label>
+						<input type="text" class="form-control" id="subscribe-name" placeholder="Your first name">
+					</div><!-- form-group -->
+					<div class="form-group">
+						<label class="sr-only" for="subscribe-email">and your email</label>
+						<input type="text" class="form-control" id="subscribe-email" placeholder="and your email">
+					</div><!-- form-group -->
+					<input type="submit" class="btn btn-danger" value="Subscribe!">
+				</form>
+				
+				<hr>
+				
+				<p><small>By providing your email you consent to receiving occasional promotional emails &amp; newsletters. <br>No Spam. Just good stuff. We respect your privacy &amp; you may unsubscribe at any time.</small></p>
+			</div><!-- modal-body -->
+			
+		</div><!-- modal-content -->
+	</div><!-- modal-dialog -->
+</div><!-- modal -->
+
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><!-- ERROR: Failed to load resource: net::ERR_FILE_NOT_FOUND - FIXED BY ADDING "http:" -->
-<script src="<?php bloginfo('template_directory') ?>/assets/js/jquery-3.4.1.min.js"></script>
-<script src="<?php bloginfo('template_directory') ?>/assets/js/old/bootstrap.min.js"></script> <!-- NB: using file from completed project since it does not display correctly with the newest version of Bootstrap  -->
-<script src="<?php bloginfo('template_directory') ?>/assets/js/main.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-3.4.1.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/old/bootstrap.min.js"></script> <!-- NB: using file from completed project since it does not display correctly with the newest version of Bootstrap  -->
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
 
 <!-- NB - a strange glitch: this code does not work from the external style sheet
 ================================================== -->
@@ -30,7 +102,7 @@
 		display: inline-block;
 		height: 40px;
 		width: 40px;
-		background: url('assets/img/icon-sprite.png') no-repeat;
+		background: url('<?php bloginfo('stylesheet_directory'); ?>/assets/img/icon-sprite.png') no-repeat;
 	}
 </style>
 
