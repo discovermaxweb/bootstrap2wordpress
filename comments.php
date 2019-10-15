f0cffae2ffed77a5fcd6ca70e2a079548959556c
Maxine Harris <discovermaxine@gmail.com>
2019-10-15 14:00:37 +0200

    Styled and customised the comments section
    Added comments wrapper from static site
    Changed comments title from h2 to h3 and edited to display "comment" instead of "thought"
    Added a few WordPress-specific styles (for individual comments)

--- comments.php
+++ comments.php
@@ -22,54 +22,59 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$bootstrap2wordpress_comment_count = get_comments_number();
-			if ( '1' === $bootstrap2wordpress_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'bootstrap2wordpress' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
-				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $bootstrap2wordpress_comment_count, 'comments title', 'bootstrap2wordpress' ) ),
-					number_format_i18n( $bootstrap2wordpress_comment_count ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
-			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
-			?>
-		</ol><!-- .comment-list -->
+	<!-- MH - added comments wrapper from static site -->
+	<div class="comments-wrap">
 
 		<?php
-		the_comments_navigation();
-
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
+		// You can start editing here -- including this comment!
+		if ( have_comments() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bootstrap2wordpress' ); ?></p>
+			<h3 class="comments-title">
+				<?php
+				$bootstrap2wordpress_comment_count = get_comments_number();
+				if ( '1' === $bootstrap2wordpress_comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html__( 'One comment on &ldquo;%1$s&rdquo;', 'bootstrap2wordpress' ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				} else {
+					printf( // WPCS: XSS OK.
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $bootstrap2wordpress_comment_count, 'comments title', 'bootstrap2wordpress' ) ),
+						number_format_i18n( $bootstrap2wordpress_comment_count ),
+						'<span>' . get_the_title() . '</span>'
+					);
+				}
+				?>
+			</h3><!-- .comments-title -->
+
+			<?php the_comments_navigation(); ?>
+
+			<ol class="comment-list">
+				<?php
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+				) );
+				?>
+			</ol><!-- .comment-list -->
+
 			<?php
-		endif;
+			the_comments_navigation();
 
-	endif; // Check for have_comments().
+			// If comments are closed and there are comments, let's leave a little note, shall we?
+			if ( ! comments_open() ) :
+				?>
+				<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'bootstrap2wordpress' ); ?></p>
+				<?php
+			endif;
 
-	comment_form();
-	?>
+		endif; // Check for have_comments().
+
+		comment_form();
+		?>
+
+	</div><!-- comments wrapper -->
 
 </div><!-- #comments -->
