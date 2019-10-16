06a93375d52fa2f506058be43c36ec7aa5779561
Maxine Harris <discovermaxine@gmail.com>
2019-10-16 15:15:29 +0200

    Customised the search results template
    In search.php:
    1. Removed page-header div and moved h1 to the top
    2. Put h1 inside the feature-image div
    3. Removed #primary and #main divs
    4. Added containers for the main content
    5. Moved sidebar and added container for it
    
    In content-search.php:
    1. Replaced entire article with the article from content.php
    
    In dashboard:
    1. Go to CPT UI -> Add/Edit Post Types -> Edit Post Types
    2. Edit the resource custom post type
    3. Under Settings, change Exclude From Search to true
    4. Save

--- search.php
+++ search.php
@@ -10,45 +10,60 @@
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+<!-- FEATURE IMAGE
+================================================== -->
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+	<h1 class="page-title">
+		<?php
+		/* translators: %s: search query. */
+		printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' );
 		?>
+	</h1>
+</section>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+<!-- MAIN CONTENT
+================================================== -->
+<div class="container">
+	<div id="primary" class="row">
+		<main id="content" class="col-sm-8">
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'search' );
+
+				endwhile;
+
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+		
+		</main><!-- #content -->
+
+		<!-- SIDEBAR
+		================================================== -->
+		<aside class="col-sm-4">
+
+			<?php get_sidebar(); ?>
+
+		</aside>
+		
+	</div><!-- #primary -->
+</div><!-- .container -->
 
 <?php
 get_sidebar();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -11,25 +11,47 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<!-- MH - edited headers to match the static site -->
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h3 class="entry-title">', '</h3>' );
+		else :
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		endif;
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			bootstrap2wordpress_posted_on();
-			bootstrap2wordpress_posted_by();
+		if ( 'post' === get_post_type() ) :
 			?>
-		</div><!-- .entry-meta -->
+
+			<!-- MH - added post details section from static site -->
+			<div class="post-details">
+				<i class="fa fa-user"></i> <?php the_author(); ?>
+				<i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
+				<i class="fa fa-folder"></i> <?php the_category(', '); ?>
+				<i class="fa fa-tags"></i> <?php the_tags(); ?>
+				
+				<div class="post-comments-badge">
+					<a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+				</div><!-- post-comments-badge -->
+
+				<!-- Add a link that allows the user to edit the post -->
+				<?php edit_post_link( 'Edit', '<i class="fa fa-pencil"></i>', '' ); ?>
+
+			</div><!-- post-details -->
+
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php bootstrap2wordpress_post_thumbnail(); ?>
+	<?php //bootstrap2wordpress_post_thumbnail(); //default starter theme template code ?>
+	<!-- MH - added feature image code from static site -->
+	<?php if ( has_post_thumbnail() ) { // check for feature image and display only if there is one ?> 
+		<div class="post-image">
+			<?php the_post_thumbnail(); ?>	
+		</div><!-- post-image -->
+	<?php } ?>
 
-	<div class="entry-summary">
+	<!-- MH - added the post excerpt container and a function to retreive the post excerpt -->
+	<div class="post-excerpt">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</div><!-- post-excerpt -->
+	
 </article><!-- #post-<?php the_ID(); ?> -->
