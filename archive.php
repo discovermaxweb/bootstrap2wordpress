a69b23e831c07649611e53118c5ec9d2d07e0634
Maxine Harris <discovermaxine@gmail.com>
2019-10-16 14:59:07 +0200

    Customised the archive section
    In archive.php:
    1. Removed #primary and #main divs
    2. Removed page-header div
    3. Enclosed archive title in the feature-image div (also edited default code for archive title and description)
    4. Added containers for the main content
    5. Moved sidebar and added container for it
    
    Edited style.css so that the archive description is more readable

--- archive.php
+++ archive.php
@@ -10,44 +10,64 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
+		<!-- FEATURE IMAGE
+		================================================== -->
+		<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+			<h1 class="page-title">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
+				the_archive_title();
 				?>
-			</header><!-- .page-header -->
+				<small class="taxonomy-description">
+					<?php
+					the_archive_description();
+					?>
+				</small>
+			</h1>
+		</section>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		<!-- MAIN CONTENT
+		================================================== -->
+		<div class="container">
+			<div id="primary" class="row">
+				<main id="content" class="col-sm-8">
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-			endwhile;
+						/*
+							* Include the Post-Type-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							*/
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_navigation();
+					endwhile;
 
-		else :
+					the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+					else :
 
-		endif;
-		?>
+						get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					endif;
+					?>
+		
+				</main><!-- #content -->
+
+				<!-- SIDEBAR
+				================================================== -->
+				<aside class="col-sm-4">
+
+					<?php get_sidebar(); ?>
+
+				</aside>
+				
+			</div><!-- #primary -->
+		</div><!-- .container -->
 
 <?php
-get_sidebar();
 get_footer();
